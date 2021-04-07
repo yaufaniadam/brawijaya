94cac399272f9a75c5e58d55c1563981ba65c837
bokerface <waskitodamar17@gmail.com>
2021-04-07 13:50:59 +0700

    .

--- ppds/app/Models/UserModel.php
+++ ppds/app/Models/UserModel.php
@@ -90,7 +90,7 @@ class UserModel extends Model
         ci_users.nama_lengkap AS nama_lengkap,
         cu.nama_lengkap AS spv,
         stase.stase AS stase,
-        tahap.tahap',
+        tahap.tahap'
         )
             ->join('tahap_ppds', 'tahap_ppds.id_user = ci_users.id')
             ->join('stase_ppds', 'stase_ppds.id_user = ci_users.id')
