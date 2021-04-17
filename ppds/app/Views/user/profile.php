962fa143e4558dc228c68463398f8904a2837a2f
bokerface <waskitodamar17@gmail.com>
2021-04-17 09:40:26 +0700

    .

--- ppds/app/Views/user/profile.php
+++ ppds/app/Views/user/profile.php
@@ -49,7 +49,7 @@
                         <div class="col-7">
                             <input type="hidden" name="id_ppds" value="<?= $data_user->id_ppds; ?>">
 
-                            <?php print_r($validation->getErrors()); ?>
+                            <?php $validation->getErrors(); ?>
 
                             <div class="form-group has-primary">
                                 <label for="inputHorizontalPrimary" class="col-form-label">Email</label>
