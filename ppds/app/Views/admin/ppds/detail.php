30c4f3bc377faf14e763dd733b37342c9d741e1e
bokerface <waskitodamar17@gmail.com>
2021-04-19 09:10:08 +0700

    .

--- ppds/app/Views/admin/ppds/detail.php
+++ ppds/app/Views/admin/ppds/detail.php
@@ -247,7 +247,6 @@
                                     </label>
                                 </div>
                                 <div class="form-group">
-                                    <label for="exampleFormControlTextarea1">Example textarea</label>
                                     <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
                                 </div>
                             </div>
