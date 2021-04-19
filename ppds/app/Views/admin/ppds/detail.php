b332a4271e743ce1a2dc27de6f7adb935ce3ad68
bokerface <waskitodamar17@gmail.com>
2021-04-19 11:19:57 +0700

    .

--- ppds/app/Views/admin/ppds/detail.php
+++ ppds/app/Views/admin/ppds/detail.php
@@ -277,6 +277,7 @@
     $('input[type=radio][name=pilih_keterangan]').change(function() {
         if ($(this).val() == 'dengan_keterangan') {
             $("#keterangan_field").show();
+            console.log($("#keterangan_field").show());
         } else {
             $("#keterangan_field").hide();
         }
