ffb30e4e23cd8b640b850c4dce2c060a9b025f7b
bokerface <waskitodamar17@gmail.com>
2021-05-03 14:18:13 +0700

    .

--- ppds/app/Views/user/profile.php
+++ ppds/app/Views/user/profile.php
@@ -226,5 +226,24 @@
         $('#password_static').replaceWith("<input type='password' nama='password' class='form-control form-control-primary' id='password'>");
         $('#photo_profile').prop('disabled', false);
     <?php } ?>
+
+    // add row
+    $("#addRow").click(function() {
+        var html = '';
+        html += '<div id="inputFormRow">';
+        html += '<div class="input-group mb-3">';
+        html += '<input type="text" name="title[]" class="form-control m-input" placeholder="Enter title" autocomplete="off">';
+        html += '<div class="input-group-append">';
+        html += '<button id="removeRow" type="button" class="btn btn-danger">Remove</button>';
+        html += '</div>';
+        html += '</div>';
+
+        $('#newRow').append(html);
+    });
+
+    // remove row
+    $(document).on('click', '#removeRow', function() {
+        $(this).closest('#inputFormRow').remove();
+    });
 </script>
 <?= $this->endSection(); ?>
