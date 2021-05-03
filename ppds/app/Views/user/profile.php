c5e5bf0e413c2e393f2557be768ab4c4e3878855
bokerface <waskitodamar17@gmail.com>
2021-05-03 14:07:46 +0700

    .

--- ppds/app/Views/user/profile.php
+++ ppds/app/Views/user/profile.php
@@ -64,9 +64,9 @@
                                     <div class="col-lg-12">
                                         <div id="inputFormRow">
                                             <div class="input-group mb-3">
-                                                <input type="text" name="title[]" class="form-control m-input" placeholder="Enter title" autocomplete="off">
+                                                <input type="text" name="title[]" class="form-control m-input mr-1" placeholder="Enter title" autocomplete="off">
                                                 <div class="input-group-append">
-                                                    <button id="removeRow" type="button" class="btn">-</button>
+                                                    <button id="removeRow" type="button" class="btn mr-1">-</button>
                                                     <button id="addRow" type="button" class="btn">+</button>
                                                 </div>
                                             </div>
