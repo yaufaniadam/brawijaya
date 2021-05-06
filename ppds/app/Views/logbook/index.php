6754b3034042fea7db6565360f3761cda663189a
bokerface <waskitodamar17@gmail.com>
2021-05-06 15:22:23 +0700

    .

--- ppds/app/Views/logbook/index.php
+++ ppds/app/Views/logbook/index.php
@@ -44,6 +44,9 @@
                                             <a href="<?= base_url("logbook/" . $logbook['id_logbook']); ?>" class="btn btn-flat btn-outline-success btn-xs">
                                                 <span class="ti-info"></span>
                                             </a>
+                                            <a href="<?= base_url("logbook/" . $logbook['id_logbook']); ?>" class="btn btn-flat btn-outline-success btn-xs">
+                                                <span class="ti-download"></span>
+                                            </a>
                                             <?php if ($logbook['id_ppds'] == session('user_id')) { ?>
                                                 <form class="d-inline" action="<?= base_url('logbook/' . $logbook['id_logbook']); ?>" method="POST">
                                                     <?= csrf_field(); ?>
