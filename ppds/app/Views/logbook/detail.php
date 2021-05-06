f011c1f0c5de47f6053a83128bd8f298300c6302
bokerface <waskitodamar17@gmail.com>
2021-05-06 12:13:30 +0700

    .

--- ppds/app/Views/logbook/detail.php
+++ ppds/app/Views/logbook/detail.php
@@ -33,7 +33,9 @@
                                             <table class="table table-borderless">
                                                 <tbody>
                                                     <tr class="text-left">
-                                                        <td><b>file</b></td>
+                                                        <td>
+                                                            <h5>file</h5>
+                                                        </td>
                                                         <td><a href="" class="btn btn-primary">Unduh</a></td>
                                                     </tr>
                                                 </tbody>
